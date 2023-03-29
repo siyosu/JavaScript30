86fac1011da4b327427134350128dbdcf4222e2a
Yosua Tarida Panjaitan <lomylix@gmail.com>
2023-03-29 23:49:24 +0700

    Day 11 - Custom Video Player

---
+++ #11 - Custom Video Player/scripts.js
@@ -0,0 +1,57 @@
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+// video.currentTime not working, and I'm tired to figure out how to fix it, so I'll just gonna leave the skip and progress function there event tho is not gonna work TT
+
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function toggleUpdate() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  toggle.innerText = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(e) {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", toggleUpdate);
+video.addEventListener("pause", toggleUpdate);
+video.addEventListener("timeupdate", handleProgress);
+toggle.addEventListener("click", togglePlay);
+window.addEventListener("keydown", (e) => {
+  if (e.code != "Space") return;
+  togglePlay();
+});
+
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => {
+  range.addEventListener("change", handleRangeUpdate);
+});
+
+progress.addEventListener("click", scrub);
